484f20bb4d25c5093be46c6843efae0593a7dba4
shinichi yamaguchi <shinichi62@gmail.com>
2015-07-01 22:55:23 +0900

    modify add local_settings file

--- web/fabfile.py
+++ web/fabfile.py
@@ -3,10 +3,14 @@
 
 from fabric.api import *
 from fabric.colors import red, yellow
+from fabric.main import load_settings
 
 import os, sys
 sys.path.append(os.pardir)
 from lib import hipchat
+
+env.rcfile = 'local_settings'
+env.update(load_settings(env.rcfile))
 
 @task
 def production(user='develop'):
@@ -35,6 +39,18 @@ def develop(user='vagrant'):
 @roles('web')
 @task
 def deploy():
+    # git clone or git pull
+    # load balancer
+    # apache stop
+    # backup
+    # deploy
+    # apache start
+    # load balancer
+    # batch
+
     run('uname -a')
+    print("repo_url: %s" % (env.repo_url))
+    print("deploy_to: %s" % (env.deploy_to))
+
     print yellow("ヒント：戻すときは以下のコマンドを実行しましょう")
     print yellow("fab %s branch:master deploy" % (env.environment))
